a4230eb548c5af2d6d4cb743ea7ed56176085b77
Thomas Sandmann <tomsing1@gmail.com>
2024-01-15 13:41:18 -0800

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,12 +20,14 @@
 ##
 golem::fill_desc(
   pkg_name = "shinyChooser", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
-  repo_url = NULL, # The URL of the GitHub Repo (optional),
+  pkg_title = "A shiny app to demonstrate how to interactively reorder factors",
+  pkg_description = paste("This R package provides a shiny web application",
+                          "that allows users to upload a tabular dataset, and",
+                          "to interactively reorder categorical variables."),
+  author_first_name = "Thomas", # Your First Name
+  author_last_name = "Sandmann", # Your Last Name
+  author_email = "tomsing1@gmail.com", # Your Email
+  repo_url = "https://github.com/tomsing1/shinyChooser",
   pkg_version = "0.0.0.9000" # The Version of the package containing the App
 )
 
@@ -37,12 +39,9 @@ golem::install_dev_deps()
 
 ## Create Common Files ----
 ## See ?usethis for more information
-usethis::use_mit_license("Golem User") # You can set another license here
+usethis::use_apache_license()
 usethis::use_readme_rmd(open = FALSE)
 devtools::build_readme()
-# Note that `contact` is required since usethis version 2.1.5
-# If your {usethis} version is older, you can remove that param
-usethis::use_code_of_conduct(contact = "Golem User")
 usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
